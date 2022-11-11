aa5f44678f923e41b4280a76c4b72b31aca00dcb
Uzair-ahmed2920 <uzairshaikh2920@gmail.com>
2022-11-11 20:26:53 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import { ThemeProvider } from '@material-ui/core';
-import './App.css';
-import MultistepForm from './components/MultistepForm';
-import theme from './theme'; 
+import { ThemeProvider } from "@material-ui/core";
+import "./App.css";
+import MultistepForm from "./components/MultistepForm";
+import theme from "./theme";
 function App() {
   return (
     <>
-    <ThemeProvider theme={theme} >
-    <MultistepForm/>
-    </ThemeProvider>
-      
+      <ThemeProvider theme={theme}>
+        <MultistepForm />
+      </ThemeProvider>
     </>
   );
 }
