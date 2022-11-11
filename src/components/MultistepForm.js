99d54b68979f2f5285b0d3d844a3b215aa0231a1
Uzair-ahmed2920 <uzairshaikh2920@gmail.com>
2022-11-11 20:26:41 +0500

    change UI
    add image on right side of form make page responsive

--- src/components/MultistepForm.js
+++ src/components/MultistepForm.js
@@ -1,4 +1,6 @@
 import React from "react";
+//import img from "../images/images.webp";
+import img from "../images/login.jpg";
 import {
   Stepper,
   Step,
@@ -11,16 +13,41 @@ import {
 } from "@material-ui/core";
 import { makeStyles } from "@material-ui/styles";
 import SignUpForm from "./SignUpForm";
-
 const useStyles = makeStyles((theme) => ({
   root: {
     width: "100%",
+    height: "100vh",
+    //backgroundColor: "#e8f5e9",
+  },
+  mainContainer: {
+    display: "grid",
+    gridTemplateColumns: "45% 55%",
+    '@media (max-width:768px)': {
+      gridTemplateColumns: "100%",
+    },
+    gridTemplateRows: "100vh",
+  },
+  imageContainer: {
+    '@media (max-width:768px)': {
+      display: "none",
+    },
+height: "100vh",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    // set the background image
+    backgroundImage: `url(${img})`,
+    backgroundSize: "cover",
   },
   backButton: {
     marginLeft: theme.spacing(14),
+    borderRadius: "20px",
+    width: "100px",
   },
   nextButton: {
     marginRight: theme.spacing(14),
+    borderRadius: "20px",
+    width: "100px",
   },
   instructions: {
     marginTop: theme.spacing(1),
@@ -31,11 +58,13 @@ const useStyles = makeStyles((theme) => ({
     textAlign: "center",
     boxShadow: "none",
     color: theme.palette.text.secondary,
+    marginTop: theme.spacing(10),
   },
   ButtonStyle: {
     justifyContent: "space-between",
     marginTop: theme.spacing(2),
   },
+  
 }));
 
 function getSteps() {
@@ -74,60 +103,64 @@ function MultistepForm() {
 
   return (
     <>
-      <Grid container justifyContent="center" className={classes.root}>
-        <Grid item xs={12} md={10}>
-          <Paper className={classes.paper}>
-            <Stepper activeStep={activeStep} alternativeLabel>
-              {steps.map((label) => (
-                <Step key={label}>
-                  <StepLabel>{label}</StepLabel>
-                </Step>
-              ))}
-            </Stepper>
-            <Container>
-              {activeStep === steps.length ? (
-                <Container>
-                  <Box className={classes.instructions}>
-                    All steps completed
-                  </Box>
-                  <Button
-                    color="secondary"
-                    variant="contained"
-                    onClick={handleReset}
-                  >
-                    Reset
-                  </Button>
-                </Container>
-              ) : (
-                <Container>
-                  <Box className={classes.instructions}>
-                    {getStepContent(activeStep)}
-                    <Grid container  className={classes.ButtonStyle}>
-                      <Button
-                        disabled={activeStep === 0}
-                        onClick={handleBack}
-                        color="secondary"
-                        variant="contained"
-                        className={classes.backButton}
-                      >
-                        Back
-                      </Button>
-                      <Button
-                        variant="contained"
-                        color="primary"
-                        onClick={handleNext}
-                        className={classes.nextButton}
-                      >
-                        {activeStep === steps.length - 1 ? "Finish" : "Next"}
-                      </Button>
-                    </Grid>
-                  </Box>
-                </Container>
-              )}
-            </Container>
-          </Paper>
+      <Box className={classes.mainContainer}>
+        <Box className={classes.imageContainer}>
+        </Box>
+        <Grid container justifyContent="center" className={classes.root}>
+          <Grid item xs={12} md={10}>
+            <Paper className={classes.paper}>
+              <Stepper activeStep={activeStep} alternativeLabel>
+                {steps.map((label) => (
+                  <Step key={label}>
+                    <StepLabel>{label}</StepLabel>
+                  </Step>
+                ))}
+              </Stepper>
+              <Container>
+                {activeStep === steps.length ? (
+                  <Container>
+                    <Box className={classes.instructions}>
+                      All steps completed
+                    </Box>
+                    <Button
+                      color="secondary"
+                      variant="contained"
+                      onClick={handleReset}
+                    >
+                      Reset
+                    </Button>
+                  </Container>
+                ) : (
+                  <Container>
+                    <Box className={classes.instructions}>
+                      {getStepContent(activeStep)}
+                      <Grid container className={classes.ButtonStyle}>
+                        <Button
+                          disabled={activeStep === 0}
+                          onClick={handleBack}
+                          color="secondary"
+                          variant="contained"
+                          className={classes.backButton}
+                        >
+                          Back
+                        </Button>
+                        <Button
+                          variant="contained"
+                          color="primary"
+                          onClick={handleNext}
+                          className={classes.nextButton}
+                        >
+                          {activeStep === steps.length - 1 ? "Finish" : "Next"}
+                        </Button>
+                      </Grid>
+                    </Box>
+                  </Container>
+                )}
+              </Container>
+            </Paper>
+          </Grid>
         </Grid>
-      </Grid>
+      </Box>
     </>
   );
 }
