f38b5b5266c3584a9aedbf06d6c7f3278caf8d0d
Uzair-ahmed2920 <uzairshaikh2920@gmail.com>
2022-11-15 19:11:19 +0500

    create  re useable component for text field

---
+++ src/components/FormInputText.js
@@ -0,0 +1,25 @@
+import TextField from "@material-ui/core/TextField";
+import { Controller } from "react-hook-form";
+import React from "react";
+
+export const FormInputText = ({ name, control, label }) => {
+  return (
+    // con troller with name, control and rules props is required for react-hook-form to work with material-ui textfield component with validation
+    <Controller 
+        name={name}
+        control={control}
+        rules={{ required: true  , minLength: 2 , maxLength: 20 }}
+        render={({ field: { onChange, value }, fieldState: { error }}) => (
+            <TextField
+            id="outlined-basic"
+                label={label}
+                variant="outlined"
+                value={value}
+                onChange={onChange}
+                error={!!error}
+                helperText={error ? error.message : null}
+            />
+        )}
+    />
+  );
+};
